9013299c96f134877a6bf3960e58c283df16bf6e
Rick Brown <ricky.brown.00@gmail.com>
2023-03-09 15:20:50 +0000

    adds dummy content

--- src/pages/trending.tsx
+++ src/pages/trending.tsx
@@ -1,5 +1,5 @@
-import { Text } from '@chakra-ui/react'
 import type { FC } from 'react'
+import Graph from '~/components/graph'
 import MainLayout from '~/components/main-layout'
 import ProtectedRoute from '~/components/protected-route'
 
@@ -9,7 +9,9 @@ const Trending: FC<TrendingProps> = (): JSX.Element => {
 	return (
 		<ProtectedRoute>
 			<MainLayout>
-				<Text>Trending</Text>
+				<Graph />
+				<Graph />
+				<Graph />
 			</MainLayout>
 		</ProtectedRoute>
 	)
