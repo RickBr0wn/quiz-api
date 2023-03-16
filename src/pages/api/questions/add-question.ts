d18266d0034def9c96bdd119d8838f7dae33d703
Rick Brown <ricky.brown.00@gmail.com>
2023-03-16 00:08:17 +0000

    adds add-question

--- src/pages/api/questions/add-question.ts
+++ src/pages/api/questions/add-question.ts
@@ -1,10 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Answer, Question } from '@prisma/client'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import _log from '~/components/log'
 import prisma from '../../../lib/prismadb'
+import jwt from 'jsonwebtoken'
 
 type _Response = {
-	message: string
+	message?: string
+	error?: string
 	question?: Question
 }
 
@@ -17,44 +20,88 @@ interface _NextApiRequest extends NextApiRequest {
 }
 
 export default async function handler(req: _NextApiRequest, res: NextApiResponse<_Response>) {
-	if (req.method !== 'POST') {
-		return res.status(405).json({
-			message: 'Method not allowed.'
-		})
-	}
+	const addNewQuestion = async () => {
+		const { question: text, answer, incorrect_answers } = req.body
+		const token = req.headers.authorization?.split(' ')[1]
 
-	const { question: text, answer, incorrect_answers } = req.body
+		if (!token) {
+			return res.status(405).json({
+				message: 'invalid authorization header'
+			})
+		}
 
-	let answers = [{ text: answer, correct: true }]
+		if (!process.env.JWT_SECRET) {
+			return res.status(500).json({ error: 'no JWT_SECRET in .env file' })
+		}
 
-	if (incorrect_answers.length != 3) {
-		return res.status(405).json({
-			message: 'The amount of incorrect answers supplied MUST be 3.'
-		})
-	}
+		const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
 
-	incorrect_answers.forEach(text => answers.push({ text, correct: false }))
+		if (!decodedToken) {
+			return res.status(500).json({ error: 'invalid token' })
+		}
 
-	try {
-		const q = await prisma.question.create({
-			include: {
-				answers: true
-			},
-			data: {
-				text,
-				answers: {
-					create: answers
-				}
+		try {
+			const user = await prisma.user.findUnique({ where: { token } })
+
+			if (!user) {
+				return res.status(405).json({
+					message: 'invalid token'
+				})
 			}
-		})
 
-		res.status(200).json({
-			message: 'Question added successfully!',
-			question: q
-		})
-	} catch (error) {
-		res.status(500).json({
-			message: `There has been an error: ${error}`
-		})
+			if (!user.isAdmin) {
+				return res.status(405).json({
+					message: 'you are not logged in as an adminstrator'
+				})
+			}
+		} catch (error) {
+			return res.status(405).json({
+				message: 'error communicating with the database'
+			})
+		}
+
+		let answers = [{ text: answer, correct: true }]
+
+		if (incorrect_answers.length != 3) {
+			return res.status(405).json({
+				message: 'The amount of incorrect answers supplied MUST be 3.'
+			})
+		}
+
+		incorrect_answers.forEach(text => answers.push({ text, correct: false }))
+
+		try {
+			const q = await prisma.question.create({
+				include: {
+					answers: true
+				},
+				data: {
+					text,
+					answers: {
+						create: answers
+					}
+				}
+			})
+
+			return res.status(200).json({
+				message: 'Question added successfully!',
+				question: q
+			})
+		} catch (error) {
+			res.status(500).json({
+				message: `There has been an error: ${error}`
+			})
+		}
+	}
+
+	switch (req.method) {
+		case 'POST':
+			addNewQuestion()
+			break
+		default:
+			res.status(500).json({
+				message: `HTTP Method not allowed`
+			})
+			break
 	}
 }
